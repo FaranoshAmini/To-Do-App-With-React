0134b1bd3471e82e5372f432dd65dc9a97e126ea
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 11:32:50 +0430

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,74 @@
-/* eslint-disable react/no-this-in-sfc */
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaTrash } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import styles from './TodoItem.module.css';
 
-function TodoItem() {
+function TodoItem({
+  id, completed, title, handleChangeProps, deleteTodoProps, setUpdate,
+}) {
+  const [editing, setEditing] = useState(false);
+
+  TodoItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+    handleChangeProps: PropTypes.func.isRequired,
+    deleteTodoProps: PropTypes.func.isRequired,
+    setUpdate: PropTypes.func.isRequired,
+  };
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  const viewMode = {};
+  const editMode = {};
+
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  useEffect(() => () => {
+  }, []);
+
   return (
-    <li>
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => handleChangeProps(id)}
+        />
+        <button type="button" onClick={() => deleteTodoProps(id)}>
+          <FaTrash />
+        </button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
       <input
-        type="checkbox"
-        checked={this.props.todo.completed}
-        onChange={() => console.log('clicked')}
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={(e) => {
+          setUpdate(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
       />
     </li>
   );
