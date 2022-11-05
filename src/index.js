153daf703cde7d9d02ac98dcebdbe4ae0089a2e3
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 14:17:29 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// component file
+import App from './functionBased/components/App';
 
-import TodoContainer from './components/TodoContainer';
+// stylesheet
+import './functionBased/App.css';
 
-ReactDom.render(
+ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
-    ,);
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
