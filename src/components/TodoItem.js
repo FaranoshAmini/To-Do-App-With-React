64d53adc54af341afae6c71f3f7962802efbbad2
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 10:04:55 +0430

    Added checkbox button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,15 @@
+/* eslint-disable react/no-this-in-sfc */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 
-function TodoItem(props) {
-  return <li>{props.todo.title}</li>;
+function TodoItem() {
+  return (
+    <li>
+      <input type="checkbox" />
+      {this.props.todo.title}
+    </li>
+  );
 }
 
 export default TodoItem;
