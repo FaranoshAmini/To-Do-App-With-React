1225bb164f44393798b333212f5597467d239aed
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-05 07:22:39 +0430

    Imported TodoItem component inTodoList

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import TodoItem from './TodoItem';
 
 class TodoList extends React.component {
   render() {
     return (
       <ul>
         {this.props.todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoItem key={todo.id} todo={todo} />
         ))}
       </ul>
     );
